fa23b541d55ea7d8eb12dd63337d4d33b86e642d
Christian Palazzo <palazzochristian@yahoo.it>
2022-12-30 00:51:36 +0100

    hashmaps

---
+++ 22.hashmaps/src/main.rs
@@ -0,0 +1,60 @@
+fn main() {
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    //accessing values
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    // iterations
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    /**
+     * For types that implement the Copy trait, like i32, 
+     * the values are copied into the hash map. For owned 
+     * values like String, the values will be moved and the 
+     * hash map will be the owner of those values
+     */
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+
+    //overwriting a value
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{:?}", scores);
+
+    // Adding a Key and Value Only If a Key Isn’t Present
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    // updating a value based on the old value
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+}
