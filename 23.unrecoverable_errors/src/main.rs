022523303a9fedc30b49a9d999abb860217b55ee
Christian Palazzo <palazzochristian@yahoo.it>
2022-12-30 01:16:41 +0100

    urecoverable errors

---
+++ 23.unrecoverable_errors/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    panic!("crash and burn");
+
+    /**
+     * we can set the RUST_BACKTRACE environment variable to 
+     * get a backtrace of exactly what happened to cause the error
+     * he RUST_BACKTRACE environment variable to any value except 0
+     */
+}
